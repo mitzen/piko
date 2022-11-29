f72d8bf5be262005c2e445e16ccdd84f7850af0e
jeremy woo <jeremy>
2022-11-29 22:13:30 +1300

    added assert method called or not

--- sqsprovidertest.py
+++ sqsprovidertest.py
@@ -2,12 +2,16 @@ import unittest
 from unittest.mock import patch
 from sqsprovider import SqsProvider
 
-@patch('sqsprovider.SqsProvider.send', return_value=[1,2])
+#@patch('sqsprovider.SqsProvider.send', return_value=[1,2])
+@patch('sqsprovider.SqsProvider')
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self, target):
-        result = SqsProvider.send("test")
+        target.send.return_value = [1,2]
+        result = target.send("test")
         self.assertTrue(result == [1,2])
-
+        target.send.assert_called_once_with("test")
+        target.receive.assert_not_called()
+    
 if __name__ == '__main__':
     unittest.main()
